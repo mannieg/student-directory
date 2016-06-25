8e36e3e356720d5e3ff782dc1d17ebc5af3fb32f
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-25 13:40:04 +0100

    output to console grouped by cohort, some misc changes based on exercise 8

--- directory.rb
+++ directory.rb
@@ -1,45 +1,66 @@
+COHORT_SYMBOLS = [:January, :February, :March, :April, :May, :June, :July,
+                  :August, :September, :October, :November, :December]
+CEN = 50
+=begin
+  Center text alignment for console output
+  Need to work on aligment, and make it more readable.
+=end
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(CEN, ' ')
+  puts "To finish, just hit return twice".center(CEN, ' ')
 
   students = []
-  cohort_symbols = [:January, :February, :March, :April, :May, :June, :July,
-                    :August, :September, :October, :November, :December]
 
-  name = gets.chomp
+  name = gets.gsub("\n","") # Using gsub instead of chomp
 
   while !name.empty? do
-    puts "What is #{name}'s country of birth"
+    cohort = get_cohort
+    puts "What is #{name}'s country of birth".center(CEN, ' ')
     country_of_birth = gets.chomp
-    puts "What is #{name}'s height"
+    puts "What is #{name}'s height".center(CEN, ' ')
     height = gets.chomp
-    puts "What is #{name}'s hobbies"
+    puts "What is #{name}'s hobbies".center(CEN, ' ')
     hobbies = gets.chomp
-    students << {name: name, cohort: :november, country_of_birth: country_of_birth,
+    students << {name: name, cohort: cohort, country_of_birth: country_of_birth,
                   height: height, hobbies: hobbies}
 
-    puts "Now we have #{students.count} student#{students.count > 1 ? 's' : ''}"
-    puts "Please provide student name or enter to exit"
+    puts "Now we have #{students.count} student#{students.count > 1 ? 's' : ''}".center(CEN, ' ')
+    puts "Please provide student name or enter to exit".center(CEN, ' ')
     name = gets.chomp
   end
   students
 end
 
+def get_cohort # Recursion if wrong input detected
+  puts "Please provide the cohort for this student".center(CEN, ' ')
+  cohort = gets.chomp.to_sym
+  cohort = :July if cohort.empty? # Default cohort if input empty
+  if !COHORT_SYMBOLS.include? cohort
+    puts("Unrecognized cohort name..").center(CEN, ' ')
+    get_cohort
+  end
+  return cohort
+end
+
 def print_header
-    puts 'The students of Villains Academy'
-    puts '-------------'
+    puts 'The students of Villains Academy'.center(CEN, ' ')
+    puts '-------------'.center(CEN, ' ')
 end
 
 def print(students)
-    i = 0
-    until i >= students.length
-        puts "#{i}.#{students[i][:name]} (#{students[i][:cohort]} cohort) -- #{students[i][:country_of_birth]}" if students[i][:name].length < 12
-        i += 1
+  if(students.length > 0)
+    by_cohort = students.group_by{|s| s[:cohort]}.each{|_,n| n.map!{|n| n[:name]}}
+    by_cohort.each do |cohort, arr| # loops through grouped hash
+      puts "The #{cohort} cohort has:".center(CEN, ' ')
+      arr.each {|name| puts "#{name}".center(CEN, ' ')} # loops through nested array
     end
+  end
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great student#{students.count > 1 ? 's' : ''}"
+    puts "Overall, we have #{students.count} great student".center(CEN, ' ') +
+         "#{students.count > 1 ? 's' : ''}"
 end
 
 students = input_students
