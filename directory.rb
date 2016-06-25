7c552bd2d0a49865f82c2257bac73863ca3f8744
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-25 18:42:10 +0100

    new interactive menu function to allow users to choose from options

--- directory.rb
+++ directory.rb
@@ -6,6 +6,28 @@ CEN = 50
   Need to work on aligment, and make it more readable.
 =end
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+   end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students".center(CEN, ' ')
   puts "To finish, just hit return twice".center(CEN, ' ')
@@ -62,7 +84,4 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great student#{students.count > 1 ? 's' : ''}".center(CEN, ' ')
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
