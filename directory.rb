06da7b72e6fe6ab2c452982d170094684aedfe21
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-25 19:07:24 +0100

    refactored code into seperate methods and renamed print method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 COHORT_SYMBOLS = [:January, :February, :March, :April, :May, :June, :July,
                   :August, :September, :October, :November, :December]
 CEN = 50
@@ -7,32 +8,40 @@ CEN = 50
 =end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-   end
+    print_menu
+    process(gets.chomp)
   end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students".center(CEN, ' ')
   puts "To finish, just hit return twice".center(CEN, ' ')
-
-  students = []
 
   name = gets.gsub("\n","") # Using gsub instead of chomp
 
@@ -44,14 +53,13 @@ def input_students
     height = gets.chomp
     puts "What is #{name}'s hobbies".center(CEN, ' ')
     hobbies = gets.chomp
-    students << {name: name, cohort: cohort, country_of_birth: country_of_birth,
+    @students << {name: name, cohort: cohort, country_of_birth: country_of_birth,
                   height: height, hobbies: hobbies}
 
-    puts "Now we have #{students.count} student#{students.count > 1 ? 's' : ''}".center(CEN, ' ')
+    puts "Now we have #{@students.count} student#{@students.count > 1 ? 's' : ''}".center(CEN, ' ')
     puts "Please provide student name or enter to exit".center(CEN, ' ')
     name = gets.chomp
   end
-  students
 end
 
 def get_cohort # Recursion if wrong input detected
@@ -70,9 +78,9 @@ def print_header
     puts '-------------'.center(CEN, ' ')
 end
 
-def print(students)
-  if(students.length > 0)
-    by_cohort = students.group_by{|s| s[:cohort]}.each{|_,n| n.map!{|n| n[:name]}}
+def print_students_list
+  if(@students.length > 0)
+    by_cohort = @students.group_by{|s| s[:cohort]}.each{|_,n| n.map!{|n| n[:name]}}
     by_cohort.each do |cohort, arr| # loops through grouped hash
       puts "The #{cohort} cohort has:".center(CEN, ' ')
       arr.each {|name| puts "#{name}".center(CEN, ' ')} # loops through nested array
@@ -80,8 +88,8 @@ def print(students)
   end
 end
 
-def print_footer(students)
-    puts "Overall, we have #{students.count} great student#{students.count > 1 ? 's' : ''}".center(CEN, ' ')
+def print_footer
+    puts "Overall, we have #{@students.count} great student#{@students.count > 1 ? 's' : ''}".center(CEN, ' ')
 end
 
 interactive_menu
