f49f5bc932a5319f104da6969c4bce005a52016e
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-25 10:48:50 +0100

    New hash values added for student, added conditional statement for student count output

--- directory.rb
+++ directory.rb
@@ -1,19 +1,27 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+
   students = []
-  # get the first names
+  cohort_symbols = [:January, :February, :March, :April, :May, :June, :July,
+                    :August, :September, :October, :November, :December]
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    puts "What is #{name}'s country of birth"
+    country_of_birth = gets.chomp
+    puts "What is #{name}'s height"
+    height = gets.chomp
+    puts "What is #{name}'s hobbies"
+    hobbies = gets.chomp
+    students << {name: name, cohort: :november, country_of_birth: country_of_birth,
+                  height: height, hobbies: hobbies}
+
+    puts "Now we have #{students.count} student#{students.count > 1 ? 's' : ''}"
+    puts "Please provide student name or enter to exit"
     name = gets.chomp
   end
-  # return the array
   students
 end
 
@@ -25,13 +33,13 @@ end
 def print(students)
     i = 0
     until i >= students.length
-        puts "#{i}.#{students[i][:name]} (#{students[i][:cohort]} cohort)" if students[i][:name].length < 12
+        puts "#{i}.#{students[i][:name]} (#{students[i][:cohort]} cohort) -- #{students[i][:country_of_birth]}" if students[i][:name].length < 12
         i += 1
     end
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{students.count} great student#{students.count > 1 ? 's' : ''}"
 end
 
 students = input_students
