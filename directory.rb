19b413f835f2d117ba2f39288761e32abd140817
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-26 11:34:57 +0100

    refactored code added File.open blocks, added abillity for user to provide file input at menu mode and console output after load or save file completion

--- directory.rb
+++ directory.rb
@@ -1,7 +1,6 @@
 @students = []
 COHORT_SYMBOLS = [:January, :February, :March, :April, :May, :June, :July,
                   :August, :September, :October, :November, :December]
-CEN = 50
 =begin
   Center text alignment for console output
   Need to work on aligment, and make it more readable.
@@ -21,9 +20,11 @@ def process(selection)
     when "2"
       show_students
     when "3"
-      save_students
+      puts "Optionally provide file name to save"
+      save_students(STDIN.gets.chomp)
     when "4"
-      load_students
+      puts "Optionally provide file name to load "
+      get_input_file(STDIN.gets.chomp)
     when "9"
       exit
     else
@@ -42,34 +43,34 @@ end
 def show_students
   print_header
   print_students_list
-  print_footer
+  student_count
 end
 
 def input_students
-  puts "Please enter the names of the students".center(CEN, ' ')
-  puts "To finish, just hit return twice".center(CEN, ' ')
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
 
   name = STDIN.gets.gsub("\n","") # Using gsub instead of chomp
 
   while !name.empty? do
     cohort = get_cohort
-    puts "What is #{name}'s country of birth".center(CEN, ' ')
+    puts "What is #{name}'s country of birth"
     country_of_birth = STDIN.gets.chomp
-    puts "What is #{name}'s height".center(CEN, ' ')
+    puts "What is #{name}'s height"
     height = STDIN.gets.chomp
     add_student(name, cohort, country_of_birth, height)
-    puts "Now we have #{@students.count} student#{@students.count > 1 ? 's' : ''}".center(CEN, ' ')
-    puts "Please provide student name or enter to exit".center(CEN, ' ')
+    student_count
+    puts "Please provide student name or enter to exit"
     name = STDIN.gets.chomp
   end
 end
 
 def get_cohort # Recursion if wrong input detected
-  puts "Please provide the cohort for this student".center(CEN, ' ')
+  puts "Please provide the cohort for this student"
   cohort = STDIN.gets.chomp.to_sym
   cohort = :July if cohort.empty? # Default cohort if input empty
   if !COHORT_SYMBOLS.include? cohort
-    puts "Unrecognized cohort name..".center(CEN, ' ')
+    puts "Unrecognized cohort name.."
     cohort = get_cohort
   end
   return cohort
@@ -81,47 +82,52 @@ def add_student(name, cohort, country_of_birth, height)
 end
 
 def print_header
-    puts 'The students of Villains Academy'.center(CEN, ' ')
-    puts '-------------'.center(CEN, ' ')
+    puts 'The students of Villains Academy'
+    puts '-------------'
 end
 
 def print_students_list
   if(@students.length > 0)
     by_cohort = @students.group_by{|s| s[:cohort]}.each{|_,n| n.map!{|n| n[:name]}}
     by_cohort.each do |cohort, arr| # loops through grouped hash
-      puts "The #{cohort} cohort has:".center(CEN, ' ')
-      arr.each {|name| puts "#{name}".center(CEN, ' ')} # loops through nested array
+      puts "The #{cohort} cohort has:"
+      arr.each {|name| puts "#{name}"} # loops through nested array
     end
   end
 end
 
-def print_footer
-    puts "Overall, we have #{@students.count} great student#{@students.count > 1 ? 's' : ''}".center(CEN, ' ')
+def student_count
+    puts "Overall, we have #{@students.count} great student#{@students.count > 1 ? 's' : ''}"
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort, country_of_birth, height = line.chomp.split(',') # Parallel assignment :)
-    add_student(name, cohort, country_of_birth, height)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort, country_of_birth, height = line.chomp.split(',') # Parallel assignment :)
+      add_student(name, cohort, country_of_birth, height)
+    end
   end
-  file.close
+  puts "#{filename} has been loaded into memory"
 end
 
-def save_students
-  file = File.open("students.csv", "w")
-
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort], student[:country_of_birth],
+def save_students(filename = "students.csv")
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort], student[:country_of_birth],
                     student[:height]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
+  puts "#{filename} has been saved.."
 end
 
-def try_load_students
+def get_argument
   filename = ARGV.first
+  get_input_file(filename)
+end
+
+def get_input_file(filename)
   if filename.nil? # leave method if nil
     puts "Loading default students.csv file..."
     load_students
@@ -131,10 +137,10 @@ def try_load_students
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist. Exiting program.."
     exit # exit program
   end
 end
 
-try_load_students
+get_argument
 interactive_menu
